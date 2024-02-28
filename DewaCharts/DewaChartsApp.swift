72f173fd56f385e03948109d61a373ec2a807987
Maneesh M <maneesh.mukundan@dewa.gov.ae>
2024-02-28 11:12:32 +0400

    Initial Commit

---
+++ DewaCharts/DewaChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DewaChartsApp.swift
+//  DewaCharts
+//
+//  Created by Maneesh on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DewaChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
